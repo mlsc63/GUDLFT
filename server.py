33e89b6487bf39a2dd626e0e4023154919b8f80f
mlescure <mlescure@mlesucre.fr>
2021-09-10 12:27:49 +0200

    bug points

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask,render_template,request,redirect,flash,url_for, session
 
 
 def loadClubs():
@@ -20,12 +20,14 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if session.get('email') is not None:
 
         club = [club for club in clubs if club['email'] == session.get('email')]
+
         return render_template('welcome.html', club=club[0], competitions=competitions)
     else:
         if request.method == "POST":
@@ -39,39 +41,69 @@ def index():
         else:
             return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
-def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+def book(competition, club):
+    if session.get('email'):
+        try:
+            foundClub = [c for c in clubs if c['name'] == club][0]
+            foundCompetition = [c for c in competitions if c['name'] == competition][0]
+            if foundClub['email'] == session.get('email'):
+                return render_template('booking.html', club=foundClub, competition=foundCompetition)
+            else:
+
+                club = [club for club in clubs if club['email'] == session.get('email')]
+                flash('you try to book for a club that is not yours')
+                return render_template('welcome.html', club=club[0], competitions=competitions)
+        except:
+            club = [club for club in clubs if club['email'] == session.get('email')]
+            flash('Club or competition not found!')
+            return render_template('welcome.html', club=club[0], competitions=competitions)
+
     else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        flash('Not connected')
+        return render_template('index.html')
+
 
 
 @app.route('/purchasePlaces',methods=['POST'])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    if session.get('email'):
+        try:
+            competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+            club = [c for c in clubs if c['name'] == request.form['club']][0]
+            placesRequired = int(request.form['places'])
+            if club['email'] == session.get('email'):
+                if club['email'] == session.get('email'):
+                    print(club['email'])
+                    if (placesRequired <= 12) and (int(competition['numberOfPlaces']) - placesRequired >= 0) and (int(club["points"]) - placesRequired >= 0):
+                        competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+                        club["points"] = int(club['points']) - placesRequired
+                        flash('Great-booking complete!')
+                        return render_template('welcome.html', club=club, competitions=competitions)
+                    else:
+                        print('pas ok')
+                        flash("You can t book")
+                        return render_template('welcome.html', club=club, competitions=competitions)
+            else:
+                flash('You try to book for another club')
+                club = [club for club in clubs if club['email'] == session.get('email')]
+                return render_template('welcome.html', club=club[0], competitions=competitions)
+        except:
+            flash("Club or competition not found")
+            club = [club for club in clubs if club['email'] == session.get('email')]
+            return render_template('welcome.html', club=club[0], competitions=competitions)
+    else:
+        flash('Not connected')
+        return render_template('index.html')
 
 
-# TODO: Add route for points display
 
+@app.route('/display')
+def display():
+    return render_template('display.html', club=clubs)
 
-@app.route('/logout')
-def logout():
-    return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
